4b2a2b6d02a1ef49becf7069aeedb20a8e114794
Eckard <eckard.appel@gmail.com>
2017-06-29 15:40:08 +0200

    rename SSH_FINGERPRINT to TF_VAR_digitalocean_ssh_fingerprint

--- export_ssh_fingerprint.bash
+++ export_ssh_fingerprint.bash
@@ -1,1 +1,1 @@
-export SSH_FINGERPRINT=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | cut -b5-)
+export TF_VAR_digitalocean_ssh_fingerprint=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | cut -b5-)
