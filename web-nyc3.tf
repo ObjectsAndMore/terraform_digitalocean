d4f08e8aecdb4eae0fbba54af4cdc17485a15c6d
Eckard <eckard.appel@gmail.com>
2017-06-29 15:41:08 +0200

    added ssh key and IPv4 address output

--- web-nyc3.tf
+++ web-nyc3.tf
@@ -1,8 +1,15 @@
 
-resource "digitalocean_droplet" "web" {
+resource "digitalocean_droplet" "terraform" {
 	image = "ubuntu-14-04-x64"
-	name = "webserver"
+	name = "terraform"
 	region = "nyc3"
 	size = "512mb"
+	ssh_keys = [
+		"${var.digitalocean_ssh_fingerprint}"
+	]
+}
+
+output "address_terraform" {
+	value = "${digitalocean_droplet.terraform.ipv4_address}"
 }
 
